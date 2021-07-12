d509f0df7f5c3e15c59b2a4d297f52a1cef3883e
Moppit <michelletranusa@gmail.com>
2021-07-12 01:35:39 -0600

    Minor format/comment fixes

--- backend/prod-php-templates/finished-module.php
+++ backend/prod-php-templates/finished-module.php
@@ -1,14 +1,32 @@
 <?php
 
-// Remember to configure each gravity form to passthe right parameters to the GET request!
+function findEntry($username) {
+    $form_id = '#'; // Create Account Form ID
+    $search_criteria = array(
+        'status'        => 'active',
+        'field_filters' => array(
+            array(
+                'key'   => '#',     // Field ID for username
+                'value' => $username,
+            ),
+        )
+    );
+    $entry = GFAPI::get_entries( $form_id, $search_criteria);
+    return $entry;
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET')
 {
-   //var_dump($_GET); //Use this to see what info we got!
-$entry_id = $_GET["<form field ID>"]; //TODO: need to change this id to the user's account!
-$field_id = '#'; // Score field
-$entry = GFAPI::get_entry( $entry_id );
-$entry[ $field_id ] = $entry[ $field_id ] + 5; // Adds by 10 for some reason...
-$result = GFAPI::update_entry( $entry );
-return $result;
+    // This is currently the entry for module user submitted
+    $field_id = '#'; // Score field
+    $resp = findEntry($_SESSION['<UN field name>']);
+    if(empty($resp)) {
+        header('Location: //www.girlsincdenver.org/bb-girls_inc_game/');
+        exit();
+    }
+    $entry = $resp[0];
+    $entry[ $field_id ] = $entry[ $field_id ] + 5;
+    $result = GFAPI::update_entry( $entry );
+    return $result;
 }
+?>
--- backend/prod-php-templates/scoreTest.php
+++ backend/prod-php-templates/scoreTest.php
@@ -2,6 +2,6 @@
 $entry_id = '#';  // Test Create Form Entry
 $field_id = '#';  // Score field
 $entry = GFAPI::get_entry( $entry_id );
-$entry[ $field_id ] = $entry[ $field_id ] + 5; // Adds by 10 for some reason...
+$entry[ $field_id ] = $entry[ $field_id ] + 5;
 $result = GFAPI::update_entry( $entry );
 return $result;
